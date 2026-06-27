5f8487abb46ad9944e85815d731fdd53dfd3bfaf
Amadou Sillah <amadousillah112@gmail.com>
2026-06-27 19:51:50 +0000

    Switch from PostgreSQL to MongoDB Atlas

--- src/main/java/com/logistics/LogisticsApplication.java
+++ src/main/java/com/logistics/LogisticsApplication.java
@@ -2,14 +2,10 @@ package com.logistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-@EnableJpaRepositories(basePackages = "com.logistics.repository")
+@SpringBootApplication
 @EnableCaching
 @EnableAsync
 public class LogisticsApplication {
