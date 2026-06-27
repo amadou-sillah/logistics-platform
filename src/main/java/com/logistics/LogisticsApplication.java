49c59fb7d7f2d41227f21fb78894668c6dea4cbe
Amadou Sillah <amadousillah112@gmail.com>
2026-06-27 05:26:31 +0000

    Remove MongoDB dependencies and exclude auto-config

--- src/main/java/com/logistics/LogisticsApplication.java
+++ src/main/java/com/logistics/LogisticsApplication.java
@@ -2,11 +2,13 @@ package com.logistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = "com.logistics.repository")
 @EnableCaching
 @EnableAsync
