a554a94929dce30a374ecbf6293b2a2b2c932ce3
Amadou Sillah <amadousillah112@gmail.com>
2026-06-28 10:55:25 +0000

    Remove DeliveryAgentRepository and WarehouseRepository

--- src/main/java/com/logistics/repository/DeliveryAgentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.logistics.repository;
-
-import com.logistics.model.DeliveryAgent;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeliveryAgentRepository extends BaseRepository<DeliveryAgent, String> {
-}
--- src/main/java/com/logistics/repository/WarehouseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.logistics.repository;
-
-import com.logistics.model.Warehouse;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WarehouseRepository extends BaseRepository<Warehouse, String> {
-}
